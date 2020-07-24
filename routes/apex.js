00e936cc25993c832ae3ef190c56b2ae55657553
enzo <enzo.seuve@laposte.net>
2020-07-24 11:55:12 +0200

    modif naming

--- routes/apex.js
+++ routes/apex.js
@@ -2,7 +2,18 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../config");
 
-// ID
+//tout les profils
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM `character`", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// consulter un personnage particulier
 router.get("/:id", (req, res) => {
   const idParams = req.params.id;
   connection.query(
